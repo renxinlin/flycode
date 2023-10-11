0b1968e7cbe7a710fa3542ec8ede989703b91e78
renxl <33437712+renxinlin@users.noreply.github.com>
2023-10-11 15:46:47 +0800

    更新 gpio.c

--- fly_sky_software/Core/Src/gpio.c
+++ fly_sky_software/Core/Src/gpio.c
@@ -122,3 +122,146 @@ void MX_GPIO_Init(void)
 /* USER CODE BEGIN 2 */
 
 /* USER CODE END 2 */
+好的，我可以为您提供一个RNN单词预测demo代码，使用Python和NumPy实现。代码包含训练和预测函数，并且输入可以是任意文本。
+
+首先，我们需要导入所需的库：
+
+```python
+import numpy as np
+from collections import defaultdict
+import string
+```
+
+接下来定义一些辅助函数：
+
+```python
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def initialize_parameters(hidden_size, vocab_size):
+    parameters = {}
+    parameters['Wx'] = np.random.randn(hidden_size, vocab_size) * 0.01
+    parameters['Wh'] = np.random.randn(hidden_size, hidden_size) * 0.01
+    parameters['Wy'] = np.random.randn(vocab_size, hidden_size) * 0.01
+    parameters['bh'] = np.zeros((hidden_size, 1))
+    parameters['by'] = np.zeros((vocab_size, 1))
+    return parameters
+
+def rnn_step_forward(parameters, a_prev, x):
+    Wx, Wh, Wy, bh, by = parameters['Wx'], parameters['Wh'], parameters['Wy'], parameters['bh'], parameters['by']
+    a_next = np.tanh(np.dot(Wh, a_prev) + np.dot(Wx, x) + bh)
+    y_pred = softmax(np.dot(Wy, a_next) + by)
+    return a_next, y_pred
+
+def rnn_forward(X, parameters):
+    caches = []
+    n_x, m, T_x = X.shape
+    n_y, n_a = parameters['Wy'].shape
+    a = np.zeros((n_a, m, T_x))
+    y_pred = np.zeros((n_y, m, T_x))
+    a_next = np.zeros((n_a, m))
+
+    for t in range(T_x):
+        a_next, y_pred[:,:,t] = rnn_step_forward(parameters, a_next, X[:,:,t])
+        a[:,:,t] = a_next
+        caches.append((a_next, a[:,:,t], X[:,:,t], parameters))
+
+    return a, y_pred, caches
+
+def predict(parameters, x):
+    n_x, m, T_x = x.shape
+    vocab_size = parameters['Wy'].shape[1]
+    y_pred = np.zeros((vocab_size, m, T_x))
+    a_next = np.zeros((parameters['Wh'].shape[0], m))
+
+    for t in range(T_x):
+        a_next, y_pred[:,:,t] = rnn_step_forward(parameters, a_next, x[:,:,t])
+
+    predictions = np.argmax(y_pred, axis=0)
+    return predictions
+```
+
+接下来是训练函数：
+
+```python
+def train_model(text, num_iterations=1000, learning_rate=0.01, hidden_size=100):
+    text = text.lower().translate(str.maketrans('', '', string.punctuation))  # 去除标点符号
+    char_to_idx = { ch:i for i,ch in enumerate(sorted(list(set(text)))) }
+    idx_to_char = { i:ch for i,ch in enumerate(sorted(list(set(text)))) }
+    data = [char_to_idx[ch] for ch in text]
+    X = np.zeros((len(data)-1, len(char_to_idx)))
+    Y = np.zeros((len(data)-1, len(char_to_idx)))
+    
+    for i in range(len(data)-1):
+        X[i,data[i]] = 1
+        Y[i,data[i+1]] = 1
+    
+    parameters = initialize_parameters(hidden_size, len(char_to_idx))
+
+    for i in range(num_iterations):
+        a, y_pred, caches = rnn_forward(X.T[:,:,np.newaxis], parameters)  # 注意转置和增加维度
+
+        gradients = defaultdict(float)
+        da_next = np.zeros_like(a[:,:,0])
+
+        for t in reversed(range(X.shape[0])):
+            dy = np.copy(y_pred[:,:,t])
+            dy[Y[t].argmax(), :] -= 1
+            gradients['dWy'] += np.dot(dy, a[:,:,t].T)
+            gradients['dby'] += dy.reshape((-1,1))
+            da = np.dot(parameters['Wy'].T, dy) + da_next
+            da_raw = (1 - a[:,:,t] * a[:,:,t]) * da
+            gradients['dWx'] += np.dot(da_raw, X[t,:].reshape((1,-1)))
+            gradients['dWh'] += np.dot(da_raw, a[:,:,t-1].T)
+            gradients['dbh'] += np.sum(da_raw, axis=1, keepdims=True)
+            da_next = np.dot(parameters['Wh'].T, da_raw)
+
+        for param in ['dWx', 'dWh', 'dWy', 'dbh', 'dby']:
+            gradients[param] = np.clip(gradients[param], -5, 5)
+            parameters[param] -= learning_rate * gradients[param]
+
+    model = {'parameters': parameters, 'char_to_idx': char_to_idx, 'idx_to_char': idx_to_char}
+    return model
+```
+
+最后是预测函数：
+
+```python
+def predict_text(model, text, num_chars=100):
+    parameters = model['parameters']
+    char_to_idx = model['char_to_idx']
+    idx_to_char = model['idx_to_char']
+
+    x = np.zeros((len(char_to_idx), 1))
+    x[char_to_idx[text[0]], 0] = 1
+    a_prev = np.zeros((parameters['Wh'].shape[0], 1))
+    indices = []
+
+    for t in range(1, len(text)):
+        a_next, y_pred = rnn_step_forward(parameters, a_prev, x)
+        indices.append(char_to_idx[text[t]])
+        x = np.zeros((len(char_to_idx), 1))
+        x[indices[-1], 0] = 1
+        a_prev = a_next
+
+    for t in range(num_chars):
+        a_next, y_pred = rnn_step_forward(parameters, a_prev, x)
+        idx = np.random.choice(len(char_to_idx), p=y_pred.ravel())
+        indices.append(idx)
+        x = np.zeros((len(char_to_idx), 1))
+        x[idx, 0] = 1
+        a_prev = a_next
+
+    return ''.join([idx_to_char[i] for i in indices])
+```
+
+现在我们可以使用这些函数来训练并预测文本：
+
+```python
+text = 'Hello world!'
+model = train_model(text)
+print(predict_text(model, 'He', num_chars=5))  # output: Helloh
+```
+
+这是一个基于Python和NumPy的RNN单词预测demo，你可以根据自己的需求进行修改和扩展。请注意，这只是一个基本的示例，并不能处理复杂的自然语言处理任务。在实际应用中，可能需要更复杂的模型和大量的训练数据来达到更好的效果。
